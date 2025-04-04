f15479932403ec27d61d8d61c319e13a99fc61b6
Avaneesh Kumar Patel <96661667+AvaneeshKumarPatel@users.noreply.github.com>
2025-04-04 16:52:09 +0530

    Update App.jsx
    Hide About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Pages/Hero';
-import About from './Components/Pages/About';
+//import About from './Components/Pages/About';
 import Projects from './Components/Pages/Projects';
 import TechStack from './Components/Pages/TechStack';
 import ContactForm from './Components/Pages/ContactForm';
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar />
       <div id="home"><Hero /></div>
-      <div id="about"><About /></div>
+{/*       <div id="about"><About /></div> */}
       <div id="projects"><Projects /></div>
       <div id="techstack"><TechStack /></div>
       <div id="contact"><ContactForm /></div>
